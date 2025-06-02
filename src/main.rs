7c633fd9a7a5846f1b7905c74ffd407f7606a81d
Magic-Forces <root@foxlab.pl>
2025-06-02 14:31:24 +0200

    Add initial implementation of stockmate CLI with config and upload commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod cli;
+mod config;
+mod upload;
+
+use clap::Parser;
+use cli::{Cli, Commands};
+
 fn main() {
-    print!("Hello World!")
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Config => {
+            config::handle_config();
+        }
+        Commands::Upload { folder } => {
+            upload::handle_upload(folder);
+        }
+    }
 }
